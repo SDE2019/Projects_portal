9bc46f874164f8c7183638c92fc2f7473ca7cb4f
ashwini krishnamurthy <ashuk2599@gmail.com>
2019-09-29 19:58:13 +0530

    Added views

---
+++ ProjectProgressMonitoringSystem/Controllers/AdminViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectProgressMonitoringSystem.Models;
+using ProjectProgressMonitoringSystem.ViewModel;
+
+namespace ProjectProgressMonitoringSystem.Controllers
+{
+    public class AdminViewController : Controller
+    {
+        // GET: AdminView
+        public ActionResult Adminview()
+        {
+            ProjectProgressMonitoringSystemEntities obj = new ProjectProgressMonitoringSystemEntities();
+            var mymodel = new TeacherStudentList();
+            mymodel.Students = obj.Students.ToList();
+            mymodel.Teachers = obj.Teachers.ToList();
+            return View(mymodel);
+        }
+    }
+}
---
+++ ProjectProgressMonitoringSystem/TeacherStudentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectProgressMonitoringSystem.Models;
+namespace ProjectProgressMonitoringSystem.ViewModel
+{
+    public class TeacherStudentList
+    {
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Teacher> Teachers { get; set; }
+    }
+}
