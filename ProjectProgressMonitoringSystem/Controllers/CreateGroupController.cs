3951b179cb6e5cf4d7bf0b305e8c8864233861d4
ashwini krishnamurthy <ashuk2599@gmail.com>
2019-10-20 17:26:27 +0530

    new changes

---
+++ ProjectProgressMonitoringSystem/Controllers/CreateGroupController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectProgressMonitoringSystem.Models;
+using ProjectProgressMonitoringSystem.ViewModel;
+namespace ProjectProgressMonitoringSystem.Controllers
+{
+    public class CreateGroupController : Controller
+    {
+        // GET: CreateGroup
+        public ActionResult CreateGroup()
+        {
+            ProjectProgressMonitoringSystemEntities obj = new ProjectProgressMonitoringSystemEntities();
+            return View(obj.Students);
+        }
+        [HttpPost]
+        public ActionResult CreateGroup(string searchTerm)
+        {
+
+            ProjectProgressMonitoringSystemEntities obj = new ProjectProgressMonitoringSystemEntities();
+            List<Student> students;
+            students = obj.Students.Where(x => x.Semester.StartsWith(searchTerm)).ToList();
+            return View(students);
+        }
+    }
+}
---
+++ ProjectProgressMonitoringSystem/ViewStudentDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectProgressMonitoringSystem.Models;
+namespace ProjectProgressMonitoringSystem.ViewModel
+{
+    public class ViewStudentDetails
+    {
+        public IEnumerable<Student> Students { get; set; }
+    }
+}
